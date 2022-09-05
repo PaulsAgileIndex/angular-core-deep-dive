f87443b54de1d35aa8141215b602b45b89541e94
frank <frank.sprich@gmail.com>
2022-09-05 15:04:34 +0200

    ...code along! - with elvis operator ?

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -27,4 +27,8 @@ export class CourseCardComponent implements OnInit {
     console.log("card component - button clicked...")
     this.courseEmitter.emit(this.course);
   }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl
+  }
 }
