3bd6eec63ee5c12897be647f17d03c6b9d4528ed
frank <frank.sprich@gmail.com>
2022-09-06 12:06:54 +0200

    ...code along! All available built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,12 @@ import {Course} from './model/course';
 export class AppComponent {
 
   courses = COURSES;
+  secondCourse = COURSES[1];
+
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.992434;
+  rate = 0.67
 
   onCourseSelected(course: Course) {
     console.log(course);
