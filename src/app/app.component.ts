47eac1bef88d03a77f1f31a511c8e88bd1f09120
frank <frank.sprich@gmail.com>
2022-09-06 12:30:51 +0200

    ...code along! View child of DOM element or Angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -12,10 +12,21 @@ export class AppComponent {
 
   courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent
+  // @ViewChild('cardRef1')
+  // card1: CourseCardComponent
+
+  @ViewChild('cardRef1', {read: ElementRef})
+  card1: CourseCardComponent
+
+  // @ViewChild('cardRef2')
+  // card2: CourseCardComponent
+
+  @ViewChild('myContainer')
+  containerDiv: ElementRef;
 
   onCourseSelected(course: Course) {
-    console.log(this.card);
+    console.log("card1: ",  this.card1);
+    // console.log("card2: ",  this.card2);
+    // console.log("containerDiv: ", this.containerDiv)
   }
 }
